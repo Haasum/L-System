a3e3cd463cd0b8ebf1e1fe0f78ecdf03a4511ee4
Nanna <nnz@ruc.dk>
2016-10-21 12:20:38 +0200

    hey hva så igen igen

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import LsysRecursive.txt;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test 2");
+        System.out.println("HEJ main");
         txt test = new txt();
         String txtFile = test.getTxtFile();
         Grammatik grammatik = new Grammatik(txtFile);
