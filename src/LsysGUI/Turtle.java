d7987caec21ef31e0468fab2cab682e7f19f510d
najacm <naja.marckwardt@gmail.com>
2016-10-21 12:23:40 +0200

    New GUI added in main and in GUI

--- src/LsysGUI/Turtle.java
+++ src/LsysGUI/Turtle.java
@@ -4,4 +4,13 @@ package LsysGUI;
  * Created by MasterWillis on 11/10/2016.
  */
 public class Turtle {
+
+    public Turtle() {
+        //the turtle constructor
+    }
+
+    public static void drawTurtle() {
+        //method for drawing
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,16 @@ package com.company;
 import LsysRecursive.Grammatik;
 import LsysRecursive.txt;
 
-public class Main {
+import javax.swing.*;
+
+public class Main extends JPanel {
 
     public static void main(String[] args) {
         System.out.println("Test 2");
         txt test = new txt();
         String txtFile = test.getTxtFile();
-        Grammatik grammatik = new Grammatik(txtFile);
+      //Grammatik grammatik = new Grammatik(txtFile); //21. okt: denne kan udkommenteres, og så kører programmet.
+        new LsysGUI.GUI();
 
 
     }
