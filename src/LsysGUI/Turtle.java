4314e486dd6d087062cc8a733d062f53785d250f
najacm <naja.marckwardt@gmail.com>
2016-10-27 14:24:15 +0200

    GUI og turtle (Naja og Lucas arbejder)

--- src/LsysGUI/Turtle.java
+++ src/LsysGUI/Turtle.java
@@ -3,20 +3,62 @@ package LsysGUI;
 import LsysRecursive.Grammatik;
 import LsysRecursive.RecursiveLsys;
 
-/**
- * Created by MasterWillis on 11/10/2016.
- */
-public class Turtle {
+import javax.swing.*;
+import java.awt.*;
+
+import static LsysGUI.GUI.mainPanel;
+
+
+public class Turtle extends JPanel {
 
     Grammatik grammatik;
     int growIs = 0;
     char c = 'A';
     private double turnIs;
+    JPanel testPanel;
 
     public Turtle(Grammatik grammatik, RecursiveLsys lsys) {
         this.grammatik = grammatik;
+
+        makeTestPanel();
         drawTurtle();
+        repaint();
     }
+
+    private void makeTestPanel() {
+        JPanel testPanel = new JPanel(){
+            public void paintComponent(Graphics g){
+                super.paintComponent(g);
+
+                g.setColor(Color.black);
+                g.drawOval(10,10,15,15);
+                g.fillOval(10,10, 15, 15);
+            }
+        };
+        //JPanel testPanel = new JPanel();
+        testPanel.setSize(600, 600);
+        testPanel.setVisible(true);
+        testPanel.setBackground(new Color(99, 125, 150));
+        testPanel.setLayout(null);
+        mainPanel.add(testPanel);
+
+
+    }
+
+
+    public void drawTurtle() {
+        System.out.println("turtle draw");
+
+
+    }
+
+
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(10 + 10, 20 + 20);
+    }
+
 
     public void setGrowIs() {
         growIs = grammatik.getGrow(c);
@@ -24,10 +66,5 @@ public class Turtle {
 
     public void setTurnIs() {
         turnIs = grammatik.getTurn(c);
-    }
-
-    public void drawTurtle() {
-        //System.out.println(growIs = grammatik.getGrow('A'));
-        //System.out.println(setTurnIs(grammatik.getTurn('A')));
     }
 }
