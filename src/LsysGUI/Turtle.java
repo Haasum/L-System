6ea2cdb1e347f1f89347ed866faee9e13ecd32a9
najacm <naja.marckwardt@gmail.com>
2016-11-01 16:02:31 +0100

    turtle er helt ændret. prøver at rykke alt inden i paintcomponent

--- src/LsysGUI/Turtle.java
+++ src/LsysGUI/Turtle.java
@@ -2,10 +2,6 @@ package LsysGUI;
 
 import LsysRecursive.Grammatik;
 import LsysRecursive.RecursiveLsys;
-import com.sun.corba.se.impl.orbutil.graph.Graph;
-import com.sun.javafx.geom.transform.Translate2D;
-import javafx.scene.transform.Transform;
-import javafx.scene.transform.Translate;
 
 import javax.swing.*;
 import java.awt.*;
@@ -37,6 +33,8 @@ public class Turtle extends JPanel {
     AffineTransform oldMatrix;
     int pushY;
     boolean pop;
+    int testHeight;
+    int testY;
 
     boolean startDraw;
     List<Shape> shapes = new ArrayList<>(); //TEST
@@ -51,25 +49,96 @@ public class Turtle extends JPanel {
         startDraw = false;
         System.out.println(growIs);
         makeTestPanel();
-        drawTurtle();
-
-
+       // drawTurtle();
 
 
     }
 
     private void makeTestPanel() {
-        JPanel testPanel = new JPanel(){
+        JPanel testPanel = new JPanel() {
 
-            public void paintComponent(Graphics g){
+            public void paintComponent(Graphics g) {
+                g.setColor(Color.BLACK);
+
+                for (int i = 0; i < drawThis.length(); i++) {
+                    char currentCheck = drawThis.charAt(i);
+
+                    //DEN RIGTIGE:
+                    if (Character.isLetter(currentCheck)) {
+                        branchHeight = 500;
+                        System.out.println(growIs);
+                    }  //TODO: det skal være med denne det skal kører
+
+                    switch (currentCheck) {
+                        case 'A':
+                            growBranch(g);
+                            break;
+                        case '+':
+                            rotateRight(g);
+                            break;
+                        case '-':
+                            rotateLeft(g);
+                            break;
+                        case '[':
+                            push(g);
+                            break;
+                        case ']':
+                            pop(g);
+                            break;
+                        default:
+                            System.out.println("Char not in alphabet");
+                            break;
+                    }
+                }
+            }
+
+            private void push(Graphics g) {
+                System.out.println("[");
+            }
+
+            private void pop(Graphics g) {
+                System.out.println("]");
+            }
+
+
+
+            private void rotateLeft(Graphics g) {
+                System.out.println("-");
+            }
+
+            private void rotateRight(Graphics g) {
+                System.out.println("+");
+            }
+
+            private void growBranch(Graphics g) {
+                System.out.println("A");
+                testHeight += 20;
+
+                g.drawLine(300, startY,300,testHeight+2);
+            }
+
+        };
+
+        testPanel.setSize(600, 600);
+        testPanel.setVisible(true);
+        testPanel.setBackground(new Color(99, 125, 150));
+        testPanel.setLayout(null);
+        mainPanel.add(testPanel);
+
+    }
+
+}
+
+
+           /*
+                SUPER PAINTCOMPONENT - DEN GAMLE - TEST
                 super.paintComponent(g);
+
 
                 Graphics2D g2d = (Graphics2D)g.create();
                 g2d.setColor(Color.red);
 
                 for (Line2D line : lines)
-
-
                 {
                    // g2d.rotate(Math.toRadians(10), startX, startY);
                     //g2d.rotate(Math.toRadians(rotationen));
@@ -108,7 +177,7 @@ public class Turtle extends JPanel {
     }
 
 
-
+/* UDKOMMENTERET FOR TEST PURPOSE
 
     public void drawTurtle() {
 
@@ -116,10 +185,10 @@ public class Turtle extends JPanel {
             char currentCheck = drawThis.charAt(i);
 
             //DEN RIGTIGE:
-           /* if (Character.isLetter(currentCheck)) {
+            if (Character.isLetter(currentCheck)) {
                     branchHeight = 500;
                     System.out.println(growIs);
-            } */ //TODO: det skal være med denne det skal kører
+            }  //TODO: det skal være med denne det skal kører
 
             switch (currentCheck) {
                 case 'A':
@@ -145,10 +214,11 @@ public class Turtle extends JPanel {
         }
     }
 
-    //METODER DER STYRER HVAD DER SKER MED TRÆET:
+    METODER DER STYRER HVAD DER SKER MED TRÆET:
 
     private void push() {
         pushIt += 60;
+        repaint();
        //saveMatrix = new AffineTransform(0,60);
         System.out.println("[");
         //lines.add(new Line2D.Double(0, 0,0, 0));
@@ -177,20 +247,15 @@ public class Turtle extends JPanel {
 
         lines.add(new Line2D.Double(startX, startY, startX, branchHeight));
     }
+    UDKOMMENTERING SLUT
+    */
 
 
 
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(10 + 10, 20 + 20);
-    }
+
 
  /*   public void setGrowIs() {
         growIs = grammatik.getGrow(c);
     } */
- //TODO: growIs skal hentes fra gramatik klassens alfabet.
+            //TODO: growIs skal hentes fra gramatik klassens alfabet.
 
-    public void setTurnIs() {
-        turnIs = grammatik.getTurn(c);
-    }
-}
