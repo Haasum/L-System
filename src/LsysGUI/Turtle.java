31b21fa9e5746edd1b03598b2df3afdd7212000c
najacm <naja.marckwardt@gmail.com>
2016-11-01 15:19:55 +0100

    Naja prøver at få turtle til at pushe og poppe rigtigt

--- src/LsysGUI/Turtle.java
+++ src/LsysGUI/Turtle.java
@@ -2,6 +2,7 @@ package LsysGUI;
 
 import LsysRecursive.Grammatik;
 import LsysRecursive.RecursiveLsys;
+import com.sun.corba.se.impl.orbutil.graph.Graph;
 import com.sun.javafx.geom.transform.Translate2D;
 import javafx.scene.transform.Transform;
 import javafx.scene.transform.Translate;
@@ -31,6 +32,11 @@ public class Turtle extends JPanel {
     int startX;
     int rotationen;
     int pushIt;
+    AffineTransform saveMatrix;
+    AffineTransform newMatrix;
+    AffineTransform oldMatrix;
+    int pushY;
+    boolean pop;
 
     boolean startDraw;
     List<Shape> shapes = new ArrayList<>(); //TEST
@@ -59,46 +65,39 @@ public class Turtle extends JPanel {
                 super.paintComponent(g);
 
                 Graphics2D g2d = (Graphics2D)g.create();
-
                 g2d.setColor(Color.red);
 
-
-
-                for (Shape shape: shapes)
-                {
-                    g2d.setColor( Color.RED );
-                    //g2d.fill( shape );
-                }
-
-                for (Integer integer : ints) {
-                   // g2d.rotate(Math.toRadians(ints.get(integer)));
-
-                }
-
-
                 for (Line2D line : lines)
+
 
                 {
                    // g2d.rotate(Math.toRadians(10), startX, startY);
                     //g2d.rotate(Math.toRadians(rotationen));
-                    AffineTransform saveMatrix;
 
-                    saveMatrix = g2d.getTransform();
-                    //g2d.getTransform();
-                    g2d.translate(0,60);
+                    saveMatrix = g2d.getTransform(); //PUSH
+                   // oldMatrix = AffineTransform.getTranslateInstance(0,0);
+
+                     //skal rykkes til push metode
+                    //pushIt += 60;
+                    newMatrix = AffineTransform.getTranslateInstance(0,pushIt);
+                    //set rotation
+                    g2d.setTransform(newMatrix);
+
+                   // g2d.translate(0,60); //ændrer nulpunkt
                     g2d.setColor(Color.black);
                     g2d.draw( line );
-                    g2d.setTransform(saveMatrix);
 
-                    //rotationen = 0;
+                    // g2d.setTransform(saveMatrix); //POP
 
                 }
+
 
                 g2d.dispose();
 
             }
+
         };
-        //JPanel testPanel = new JPanel();
+
         testPanel.setSize(600, 600);
         testPanel.setVisible(true);
         testPanel.setBackground(new Color(99, 125, 150));
@@ -107,6 +106,8 @@ public class Turtle extends JPanel {
 
 
     }
+
+
 
 
     public void drawTurtle() {
@@ -122,56 +123,62 @@ public class Turtle extends JPanel {
 
             switch (currentCheck) {
                 case 'A':
-
-                    System.out.println("A");
-                    branchHeight = 50; //Getbranch height
-                    startX = 300;
-
-                    lines.add(new Line2D.Double(startX, startY, startX, branchHeight));
-
+                    growBranch();
                     break;
                 case '+':
-                    System.out.println("+");
-                    //rotationen = 0;
-                   // ints.add(new Integer(10));
+                    rotateRight();
                     break;
-
                 case '-':
-                    //rotationen = -10;
-                    System.out.println("-");
-                   // ints.add(new Integer(-10));
+                    rotateLeft();
                     break;
                 case '[':
-                    //translates.add(new Translate(0.0,0.0));
-                    System.out.println("[");
-                    //startY += branchHeight + 10;
+                    push();
                     break;
                 case ']':
-                    //startY -= branchHeight - 10;
-                    System.out.println("]");
+                    pop();
                     break;
                 default:
                     System.out.println("Char not in alphabet");
                     break;
             }
 
-
-
-            //List<Translate2D> translates = new ArrayList<>();
-
-       /* for(int i = 0; i < 10; i++) {
-
-
-            lang += 5;
-            start2 += 10;
-            shapes.add( new Ellipse2D.Double(lang, 50, 50, 50) ); //TEST
-
-            lines.add( new Line2D.Double(start2,lang,50,50));
-
-        } */
-
         }
     }
+
+    //METODER DER STYRER HVAD DER SKER MED TRÆET:
+
+    private void push() {
+        pushIt += 60;
+       //saveMatrix = new AffineTransform(0,60);
+        System.out.println("[");
+        //lines.add(new Line2D.Double(0, 0,0, 0));
+    }
+
+    private void pop() {
+        //pushIt = 0;
+        pop = true;
+        System.out.println("]");
+    }
+
+
+
+    private void rotateLeft() {
+        System.out.println("-");
+    }
+
+    private void rotateRight() {
+        System.out.println("+");
+    }
+
+    private void growBranch() {
+        System.out.println("A");
+        branchHeight = 50; //Getbranch height
+        startX = 300;
+
+        lines.add(new Line2D.Double(startX, startY, startX, branchHeight));
+    }
+
+
 
     @Override
     public Dimension getPreferredSize() {
